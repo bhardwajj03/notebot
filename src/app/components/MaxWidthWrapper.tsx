19c135e2da0bbb66a9d99e15dd2cdbf1a48ffa3d
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-08-08 11:59:21 +0530

    setup

---
+++ src/app/components/MaxWidthWrapper.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+const MaxWidth=({
+    className,
+    children
+}:{
+    className?:string
+    children:ReactNode
+})=>{
+    <div></div>
+}
+
+export default MaxWidth
