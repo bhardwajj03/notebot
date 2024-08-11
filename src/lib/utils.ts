24e63db0c4796ba6c030dfc86166aacb0bc84cc5
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-08-11 17:46:32 +0530

    tailwind confs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,8 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs:ClassValue[]){
+
+    // tw do is it merges the styling like px-2 py-2 --> p-2  //
     return twMerge(clsx(inputs))
 }
