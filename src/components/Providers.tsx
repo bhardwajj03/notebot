b6a051e989e8df1530db7a7fb7663d53c25b018d
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-08-20 20:58:24 +0530

    trpc folder management

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,4 +1,13 @@
-const Providers=()=>{}
+"use client"
+
+import { useState } from "react"
+import {QueryClient} from "@tanstack/react-query"
+
+const Providers=()=>{
+
+    const [queryClient]=useState(()=> new QueryClient( ))
+
+}
 
 
 export default Providers 
