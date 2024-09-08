e1e4d705ba1af686ad1aaaec07512629e7aa0cea
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-09-08 23:04:33 +0530

    hd

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -6,18 +6,18 @@ import { redirect } from "next/dist/server/api-utils";
 
 export const  appRouter=router({
     authCallback:publicProcedure.query( async()=>{
-        const {getUser} = getKindeServerSession()
-        const user= await getUser()
+        const {getUser} = getKindeServerSession();
+        const user= await getUser();
 
-        if(!user.id || !user.email)
+        if(!user || !user.id || !user.email)
             throw new TRPCError({code:'UNAUTHORIZED'})
 
         // check if the user is in the database 
         const dbUser =  await db.user.findFirst({
             where:{
                 id:user.id
-            }
-        })
+            },
+        });
 
         if (!dbUser) {
             // it means user is not in database so we have to make user to register in database 
@@ -26,16 +26,116 @@ export const  appRouter=router({
                 data:{
                     id:user.id,
                     email:user.email
-                }
-            })
+                },
+            });
         }
 
 
 
-        return {success :true}
+        return {success :true};
     }),
     
 
 });
 
 export type appRouter = typeof appRouter;
+
+// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+// import {
+//   privateProcedure,
+//   publicProcedure,
+//   router,
+// } from './trpc';
+// import { TRPCError } from '@trpc/server';
+// import { db } from '@/db';
+// import {z} from 'zod';
+
+// export const appRouter = router({
+//   authCallback: publicProcedure.query(async () => {
+//     const { getUser } = getKindeServerSession();
+//     const user = await  getUser(); // Await the getUser function
+
+//     if (!user || !user.id || !user.email) {
+//       throw new TRPCError({ code: 'UNAUTHORIZED' });
+//     }
+
+//     // Check if the user is in the database
+//     const dbUser = await db.user.findFirst({
+//       where: {
+//         id: user.id,
+//       },
+//     });
+
+//     if (!dbUser) {
+//       // Create user in db
+//       await db.user.create({
+//         data: {
+//           id: user.id,
+//           email: user.email,
+//         },
+//       });
+//     }
+
+//     return  { success: true };
+     
+    
+//   }),
+//   getUserFiles: privateProcedure.query(async({ ctx }) => {
+//     const { userId } = ctx
+
+//     return await db.file.findMany({
+//       where: {
+//         userId,
+//       },
+//     })
+//   }),
+ 
+//   getFile: privateProcedure.input(z.object({ key: z.string()})).mutation(async({ctx,input})=>{  
+//     const {userId} = ctx
+//     const file = await db.file.findFirst({
+//       where: {
+//         key: input.key,
+//         userId,
+//       },
+//     })
+//     if(!file){
+//       throw new TRPCError({ code: 'NOT_FOUND'})
+//     }
+//     return file
+
+//   }),
+
+
+
+//   deleteFile : privateProcedure.input(
+//     z.object({ id: z.string()})
+//   ).mutation(async({ ctx ,input }) => {
+//      const {userId } = ctx
+
+//      const file = await db.file.findFirst({
+//       where: {
+//         id :input.id,
+//         userId,
+//       },
+//      })
+//      if(!file){
+//        throw new TRPCError({ code: 'NOT_FOUND'})
+//      }
+//       await db.file.delete({
+//         where: {
+//           id: input.id,
+
+//         },
+//       })
+//       return file
+
+//   })
+  
+
+  
+  
+// })
+
+// // Export type router type signature,
+// // NOT the router itself.
+// export type AppRouter = typeof appRouter;
