0c421142c1e94306bad7aa7e6441342c1c55014b
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-09-11 18:51:51 +0530

    perefecting our dashboard

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -1,20 +1,39 @@
-
+import { Ghost } from "lucide-react"
+import { trpc } from "../_trpc/client"
 import UploadButton from "./UploadButton"
 
 const Dashboard=()=>{
+
+    const {data:files , isLoading}=trpc.getUserFiles.useQuery()
+
     return(
-        <div className="text-red-800" >hello</div>
-        // <>
-        //     <main className="mx-auto max-w-7xl md:p-10">
-        //     <div className="mt-8 flex flex-col items-start justify-between
-        //      gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
-        //         <h1 className="mb-3 font-bold text-5xl text-gray-900">
-        //             My Files 
-        //         </h1>
-        //         <UploadButton />
-        //      </div>
-        // </main>
-        // </>
+            <main className="mx-auto max-w-7xl md:p-10">
+            <div className="mt-8 flex flex-col items-start justify-between
+             gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
+                <h1 className="mb-3 font-bold text-5xl text-gray-900">
+                    My Files 
+                </h1>
+                <UploadButton />
+             </div>
+
+            {/* Display all the user files */}
+
+            {files && files?.length!==0 ?(
+                <div></div>
+            ): isLoading ?(
+                <div></div>
+            ) : (
+                <div className="mt-16 flex flex-col items-center gap-2">
+                    <Ghost className="h-8 w-8 text-zinc-800" />
+                    <h3 className="font-semibold text-xl">
+                        Pretty empty around here
+                    </h3>
+                    <p>Let&apos;s upload first PDF.</p>
+                </div>
+            )}
+
+        </main>
+        
     )
 }
 
