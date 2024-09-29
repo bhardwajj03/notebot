c3c6c3f1e545ab7e5e5e029e7a949b6a18c8ac70
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-09-30 00:01:03 +0530

    creating our file uploader

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -1,14 +1,37 @@
 "use client"
 
 
-import { Dialog,DialogTrigger } from "./ui/dialog"
+import { Dialog,DialogContent,DialogTrigger } from "./ui/dialog"
 import { Button } from "./ui/Button"
 import { useState } from "react"
-
+import Dropzone from 'react-dropzone'
+import { Cloud } from "lucide-react"
 
 const UploadButton=()=>{
     const [isOpen,setIsOpen]=useState<boolean>(false)
      
+    const UploadDropzone =()=>{
+       return (
+            <Dropzone multiple={false} onDrop={(acceptedFiles)=>{
+                console.log(acceptedFiles)
+            }}>
+                {({getRootProps,getInputProps,acceptedFiles})=>(
+                    <div {...getRootProps()}
+                    className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
+                        <div className='flex items-center justify-center h-full w-full'>
+                            <label
+                                htmlFor="dropzone-file"
+                                className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                                    <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                                        <Cloud /></div>
+                                </label>
+                        </div>
+                    </div>
+                )}
+            </Dropzone>
+       )
+    }
+
     return(
         <Dialog 
             open={isOpen}
@@ -25,6 +48,10 @@ const UploadButton=()=>{
                 asChild>
                 <Button>Upload PDF</Button>
             </DialogTrigger>
+
+            <DialogContent>
+                <UploadDropzone />
+            </DialogContent>
         </Dialog>
     )
 }
