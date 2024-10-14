122dab12d549d9f1b4cbdac3417ac0b1e148abdb
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-10-14 20:52:42 +0530

    shs

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -9,9 +9,31 @@ import { Cloud } from "lucide-react"
 
 
 const UploadDropzone=()=>{
-    return <Dropzone multiple={false}>
+    return <Dropzone multiple={false} onDrop={(acceptedFile)=>{
+        console.log(acceptedFile) 
+    }}>
         {({getRootProps,getInputProps,acceptedFiles})=>(
-            <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-400 rounded-lg"></div>
+            <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-400 rounded-lg">
+                <div className="flex items-center justify-center h-full w-full">
+                    <label htmlFor="dropzone-file" className="flex felx-col items-center justify-center w-full h-full rounded-lg cusrsor-pointer bg-gray-50 hover:bg-gray-150">
+                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            <Cloud className="h-6 w-6 text-zinc-500 mb-2"/>
+                            <p className="mb-2 text-sm text-zinc-700">
+                                <span className="font-semibold">Click to Upload</span>{' '}or drag and drop
+                            </p>
+                            <p className="tex-xs text-zinc-500">PDF (Up to 4MB)</p>
+                        </div>    
+                        {acceptedFiles && acceptedFiles[0] ? ( 
+                            <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200"> 
+                                <div>
+                                    
+                                </div>
+                            </div>
+                        ):null}   
+
+                    </label>
+                </div>
+            </div>
         )}
     </Dropzone>
 }
@@ -19,33 +41,7 @@ const UploadDropzone=()=>{
 const UploadButton=()=>{
     const [isOpen,setIsOpen]=useState<boolean>(false)
      
-    const UploadDropzone =()=>{
-
-        const [isUploading,SetIsUploading]=useState<boolean>(true)
-        const [uploadProgress,SetUploadProgress]=useState<number>(0)
-
-
-
-       return (
-            <Dropzone multiple={false} onDrop={(acceptedFiles)=>{
-                console.log(acceptedFiles)
-            }}>
-                {({getRootProps,getInputProps,acceptedFiles})=>(
-                    <div {...getRootProps()}
-                    className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
-                        <div className='flex items-center justify-center h-full w-full'>
-                            <label
-                                htmlFor="dropzone-file"
-                                className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
-                                    <div className='flex flex-col items-center justify-center pt-5 pb-6'>
-                                        <Cloud /></div>
-                                </label>
-                        </div>
-                    </div>
-                )}
-            </Dropzone>
-       )
-    }
+    
 
     return(
         <Dialog 
