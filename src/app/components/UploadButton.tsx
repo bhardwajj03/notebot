8edd115c6ed4f4fe268348b88b513acf444cb44e
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-10-15 21:09:26 +0530

    finished file uploader

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -13,9 +13,34 @@ const UploadDropzone=()=>{
     const [isUploading,SetIsUploading]=useState<boolean>(true)
     const [uploadProgress,SetUploadProgress]=useState<number>(0)  
 
+    const startSimulatedProgress=()=>{
+        SetUploadProgress(0)
 
-    return <Dropzone multiple={false} onDrop={(acceptedFile)=>{
-        console.log(acceptedFile) 
+        const interval=setInterval(()=>{
+            SetUploadProgress((prevProgress)=>{
+                if(prevProgress >=95){
+                    clearInterval(interval)
+                    return prevProgress
+                }
+                return prevProgress + 5
+            })
+        },500)
+
+        return interval
+    }
+
+    return <Dropzone multiple={false} onDrop={async(acceptedFile)=>{
+        SetIsUploading(true)
+
+        const progressInterval=startSimulatedProgress()
+
+        // handle the file uploading
+
+        await new Promise((resolve)=>setTimeout(resolve,1500))
+        //after uploading we can clear the interval
+        clearInterval(progressInterval)
+        SetUploadProgress(100)
+
     }}>
         {({getRootProps,getInputProps,acceptedFiles})=>(
             <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-400 rounded-lg">
@@ -42,7 +67,7 @@ const UploadDropzone=()=>{
 
                         {isUploading ? (
                             <div className="w-full mt-4 max-w-xs mx-auto">
-                                <Progress value={50} className="h-1 w-full text-zinc-200" />
+                                <Progress value={uploadProgress} className="h-1 w-full text-zinc-200" />
                             </div>
                         ) :null} 
 
